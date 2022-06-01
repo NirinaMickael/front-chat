fc78c22f0cbcdbd2872064bb20096ca68a0781a6
NirinaMickael <todisoanirinamickael@gamil.com>
2022-06-01 13:07:36 +0300

    interface fetchState

--- src/app/pages/page-user/core/service/user.service.ts
+++ src/app/pages/page-user/core/service/user.service.ts
@@ -1,15 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, map, Observable } from 'rxjs';
 import { AuthService } from 'src/app/core/service/auth.service';
+import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
+  user$ = new BehaviorSubject<any>('');
+  convesation$ = new BehaviorSubject<boolean>(false);
   frappe$ = new BehaviorSubject<any>('');
   constructor(private _httpClient: HttpClient, private _user: AuthService) {}
   searchUser(user: string): Observable<any> {
-    return this._httpClient.get(`http://localhost:3000/api/${user}`);
+    return this._httpClient.get(environment.api+'/api/'+user);
   }
   dataUser(url : string,option={}): Observable<any> {
     return this._user.getUser(url,option);
@@ -17,4 +20,28 @@ export class UserService {
   AddFriend(id : string){
     console.log("user is added")  
   }
+  getConversation(url: string): Observable<any> {
+    return this._httpClient.get(url);
+  }
+  viewMessage(url: string): Observable<any> {
+    return this._httpClient.get(url);
+  }
+  getAllUser(url: string): Observable<any> {
+    return this._httpClient.get(url);
+  }
+  getImage(url: string, option = {}): Observable<any> {
+    return this._httpClient.get(url, option).pipe(
+      map( (data: any) => this._arrayBufferToBase64(data))
+    )
+  }
+  _arrayBufferToBase64( buffer : any) : string {
+    var binary = '';
+    var bytes = new Uint8Array( buffer );
+    var len = bytes.byteLength;
+    for (var i = 0; i < len; i++) {
+       binary += String.fromCharCode( bytes[ i ] );
+    }
+    return window.btoa( binary );
+  }
 }
+
--- src/app/pages/page-user/home/home.component.ts
+++ src/app/pages/page-user/home/home.component.ts
@@ -1,4 +1,7 @@
+import { FetchState } from './../core/models/FetchState';
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { UserService } from '../core/service/user.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  userId !: string | null;
+  conversation: any;
+  user: any;
+  stateUser  : FetchState = {
+    isLoading: true,
+    isPedding: true,
+    isFinish: false
+  };
+  constructor(private _user: UserService) {
   }
-
+  ngOnInit(): void {
+    // console.log(this.stateUser) 
+    this.userId = sessionStorage.getItem('id');
+    this._user.dataUser(environment.api + "/api/user/" + this.userId).subscribe(
+      data => {
+        this.stateUser.isLoading = false;
+        this.stateUser.isFinish = true;
+        this.user = data;
+        this._user.user$.next(data);
+      }
+    );
+  }
 }
