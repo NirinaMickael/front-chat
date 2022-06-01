6ebe852354d585f6eec6c409149d9c2374869af9
NirinaMickael <todisoanirinamickael@gamil.com>
2022-06-01 13:17:41 +0300

    headerChat

--- src/app/pages/page-user/home/all-conversation/all-conversation.component.ts
+++ src/app/pages/page-user/home/all-conversation/all-conversation.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { FetchState } from './../../core/models/FetchState';
+import { filter, map } from 'rxjs';
+
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { UserService } from '../../core/service/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-all-conversation',
@@ -6,10 +12,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./all-conversation.component.scss']
 })
 export class AllConversationComponent implements OnInit {
-
-  constructor() { }
+  @Input() userId !: string;
+  @Output() messageBox = new EventEmitter<any>();
+  allFriends !: any ;
+  allFriendState : FetchState = {
+    isLoading : true,
+    isFinish :false,
+    isPedding:true
+  };
+  index : number = 0;
+  constructor(private _user :  UserService , private _route : Router) { }
 
   ngOnInit(): void {
+    this._user.getAllUser(environment.api+"/api/alluser").pipe(
+      map(res => res.filter((data: any)=>data._id != this.userId))
+    ).subscribe(
+      data => {
+        this.allFriendState.isLoading = false;
+        this.allFriendState.isFinish = true;
+        this.allFriends =  data
+        console.log(this.allFriends)
+      }
+    )
+  }
+  handleClick(username: string) {
+    // this._route.navigate(["/pages/conversation",username]);
+    this.messageBox.emit(username);
   }
 
 }
--- src/app/pages/page-user/home/header-chat/header-chat.component.ts
+++ src/app/pages/page-user/home/header-chat/header-chat.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
-import { delay, map } from 'rxjs';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { environment } from 'src/environments/environment';
 import { UserService } from '../../core/service/user.service';
 
 @Component({
@@ -8,12 +9,30 @@ import { UserService } from '../../core/service/user.service';
   styleUrls: ['./header-chat.component.scss']
 })
 export class HeaderChatComponent implements OnInit {
-  constructor(private _user : UserService) { }
+  constructor(private _user : UserService , private sanitizer : DomSanitizer) { }
   user !:any;
+  imagePath !: SafeResourceUrl;
+  isLoad  = false;
+  option = {
+    responseType : "ArrayBuffer"
+  }
   ngOnInit(): void {
-    const id = sessionStorage.getItem('id');
-    this._user.dataUser(`http://localhost:3000/api/user/${id}`).pipe(
-      delay(100)
-    ).subscribe(data=>this.user=data)
+    this._user.user$.subscribe(
+      data => this.user = data
+    )
+    setTimeout(()=>{
+      this._user.getImage(environment.api+"/api/getImage/"+this.user._id,this.option).subscribe(
+        data => {
+          this.isLoad = false;
+          let objectURL = 'data:image/jpeg;base64,' + data;
+
+         this.imagePath = this.sanitizer.bypassSecurityTrustUrl(objectURL);
+        }
+      )
+    },500)
+  }
+  sanitize(url : string){
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
   }
 }
+
